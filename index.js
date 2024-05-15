881c9b5dcaad75e513f83dd451c545bed416c02a
Jean Moreira <jeancm110@gmail.com>
2024-05-15 18:22:01 -0300

    Atualizadas as mensagens do sistemas

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usando o github");
 turboLogger("estou aprendendo o fluxo do git");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão do sistema!",
+  "darkgreen"
+);
